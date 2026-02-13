c104b5f423fb3b90136f268147971c989d7a0535
ramims2026-bit <ramims2026@gmail.com>
2026-02-13 06:01:24 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // הסרנו את ה-experimental.runtime
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
 };
-
 export default nextConfig;
